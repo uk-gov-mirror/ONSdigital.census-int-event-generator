15fee50c9240b4936606372c62265be1b894c516
Phil Whiles <phil.whiles@gmail.com>
2019-12-16 12:30:56 +0000

    CR-000 travis simplification

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,16 +2,8 @@
 
 echo $GCLOUD_SERVICE_KEY | base64 -d | docker login -u _json_key --password-stdin https://eu.gcr.io
 
-export VERSION=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
-export VERSIONTAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo $VERSION; else echo $VERSION"-"$TRAVIS_PULL_REQUEST_BRANCH; fi`
-export RELEASETAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo "latest"; fi`
-
-echo "Building with tags [$VERSIONTAG $RELEASETAG]"
+export VERSIONTAG=$TRAVIS_BUILD_ID"-"$TRAVIS_BRANCH
+echo "Building with tags [$VERSIONTAG]"
 
 docker build -t eu.gcr.io/census-int-ci/census-event-generator:$VERSIONTAG .
 docker push eu.gcr.io/census-int-ci/census-event-generator:$VERSIONTAG
-
-if [ ! -z "$RELEASETAG" ]; then
-  docker tag eu.gcr.io/census-int-ci/census-event-generator:$VERSIONTAG eu.gcr.io/census-int-ci/census-event-generator:$RELEASETAG
-  docker push eu.gcr.io/census-int-ci/census-event-generator:$RELEASETAG
-fi
